bd6a4867d5f394d917caee1f50667f0c313ba54c
Hong Jinho <tpdleps@gmail.com>
2020-05-03 00:00:00 +0900

    add test complete

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -7,32 +7,54 @@ import {
   PercentDiscountPolicy,
   Screening,
 } from "../src/chapter2";
-test("This is a sample", () => {
-  const avatar = new Movie(
-    "avatar",
-    120,
-    Money.wons(10000),
-    new AmountDiscountPolicy(
-      Money.wons(800),
-      new SequenceCondition(1),
-      new SequenceCondition(10),
-      new PeriodCondition(1, new Date(), new Date()),
-      new PeriodCondition(1, new Date(), new Date())
-    )
-  );
-  console.log(avatar.calculateMovieFee(new Screening(avatar, 1, new Date())));
-
-  const titanic = new Movie(
-    "titanic",
-    180,
-    Money.wons(11000),
-    new PercentDiscountPolicy(
-      0.1,
-      new PeriodCondition(2, new Date(), new Date()),
-      new SequenceCondition(2),
-      new PeriodCondition(4, new Date(), new Date())
-    )
-  );
-  console.log(titanic.calculateMovieFee(new Screening(titanic, 2, new Date())));
-  expect(true).toBe(true);
+describe("This is a sample", () => {
+  it("금액 할인만 받고 기간 할인에는 일치 하지 않을 때", () => {
+    //GIVEN
+    const avatar = new Movie(
+      "avatar",
+      120,
+      Money.wons(10000),
+      new AmountDiscountPolicy(
+        Money.wons(800),
+        new SequenceCondition(1),
+        new SequenceCondition(10),
+        new PeriodCondition(
+          1,
+          new Date(2000, 1, 1, 0, 0, 0),
+          new Date(2000, 1, 3, 0, 0, 0)
+        )
+      )
+    );
+    const now = new Date(2000, 1, 2, 0, 0, 0);
+    const sequence = 1;
+    //WHEN
+    const fee = avatar.calculateMovieFee(new Screening(avatar, sequence, now));
+    //THEN
+    expect(fee.amount).toEqual(9200);
+  });
+  it("퍼센트 할인의 기간이 맞았을", () => {
+    //GIVEN
+    const titanic = new Movie(
+      "titanic",
+      180,
+      Money.wons(10000),
+      new PercentDiscountPolicy(
+        0.1,
+        new PeriodCondition(
+          3,
+          new Date(2000, 1, 1, 0, 0, 0),
+          new Date(2000, 1, 3, 0, 0, 0)
+        ),
+        new SequenceCondition(2)
+      )
+    );
+    const now = new Date(2000, 1, 2, 0, 0, 0);
+    const sequence = 1;
+    //WHEN
+    const fee = titanic.calculateMovieFee(
+      new Screening(titanic, sequence, now)
+    );
+    //THEN
+    expect(fee.amount).toEqual(9000);
+  });
 });
