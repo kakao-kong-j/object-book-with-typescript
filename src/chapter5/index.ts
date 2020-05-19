d173c7d136fff88bd7102d61000f417d64042491
Hong Jinho <tpdleps@gmail.com>
2020-05-19 22:28:26 +0900

    Screening은 `예매하라` 라는 메시지를 처리할 수 있어야 합니다.
    해당 책임을 수행하는데 필요한 정보를 인스턴스 변수로 포함합니다.

---
+++ src/chapter5/index.ts
@@ -0,0 +1,11 @@
+export class Screening {
+  private movie: Movie;
+  private sequence: number;
+  private whenScreened: Date;
+
+  public reserve(customer: Customer, audienceCount: number) {}
+}
+
+class Movie {}
+
+class Customer {}
