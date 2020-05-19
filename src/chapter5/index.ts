9bd8d0786cc3eaa75aee7dd37e5139bb54572548
Hong Jinho <tpdleps@gmail.com>
2020-05-19 22:35:33 +0900

    메시지의 송신자인 Screening의 의도를 표현한다.
    송신자를 먼저 작성함으로써 수신자와 송신자의 관계를 메시지로만 할 수 있도록 설계한다. 그로인해 수신자의 설계가 간단해 진다

--- src/chapter5/index.ts
+++ src/chapter5/index.ts
@@ -3,9 +3,26 @@ export class Screening {
   private sequence: number;
   private whenScreened: Date;
 
-  public reserve(customer: Customer, audienceCount: number) {}
+  public reserve(customer: Customer, audienceCount: number) {
+    return new Reservation(
+      customer,
+      this,
+      this.calculateFee(audienceCount),
+      audienceCount
+    );
+  }
+
+  private calculateFee(audienceCount: number): Money {
+    return this.movie.calculateMovieFee(this).times(audienceCount);
+  }
 }
 
-class Movie {}
+class Movie {
+  public calculateMovieFee(screening: Screening): Money {}
+}
 
 class Customer {}
+
+class Reservation {}
+
+class Money {}
